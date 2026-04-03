540ca6d6ea5e692af7ca25ca45766f46e3ca7177
robinnayak <robinnayak86@gmail.com>
2026-04-03 18:31:13 +0545

    fix: set default sqlite database URI in config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///todo.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
